3342ec474443a831d5dfb2b7d7d5d00935a718de
big <borja.igartua@appsfactory.de>
2019-02-27 15:21:35 +0100

    add action constraint to match

---
+++ backend/Code/Controllers/ForecastController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Code.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace backend.Code.Controllers {
+    [Route("api/weather/[controller]")]
+    public class ForecastController : Controller {
+
+        [HttpGet()]
+        [QueryStringConstraint("city", true)]
+        [QueryStringConstraint("zipCode", false)]
+        public async Task<IActionResult> ForecastByCity(string city) {
+        
+            return Ok();
+        }
+
+        [HttpGet()]
+        [QueryStringConstraint("zipCode", true)]
+        [QueryStringConstraint("city", false)]
+        public async Task<IActionResult> ForecastByZip([FromQuery(Name = "zipCode")] string zip) {
+
+            return Ok();
+        }
+    }
+}
---
+++ backend/Code/Utils/QueryStringConstraintAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.AspNetCore.Routing;
+
+namespace backend.Code.Utils {
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple =true)]
+    public class QueryStringConstraintAttribute: ActionMethodSelectorAttribute {
+
+        private string Name { get; set; }
+        private bool Present { get; set; }
+
+        public QueryStringConstraintAttribute(string name, bool present) {
+            this.Name = name;
+            this.Present = present;
+        }
+
+        public override bool IsValidForRequest(RouteContext routeContext, ActionDescriptor action) {
+            var value = routeContext.HttpContext.Request.Query[this.Name];
+            if (this.Present) {
+                return !String.IsNullOrEmpty(value);
+            }
+
+            return String.IsNullOrEmpty(value);
+        }
+    }
+}
